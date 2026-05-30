83ea12cbf02344226c94aa47cfb34642428f91a9
Akram Mohammed <42640947+Akram-Mohammed-J@users.noreply.github.com>
2026-05-30 13:57:21 +0530

    Add: binary-search - submission-4

---
+++ Data Structures & Algorithms/binary-search/submission-4.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let l = 0;
+        let h = nums.length - 1;
+
+        while (l <= h) {
+            let mid = Math.floor((l + h) / 2);
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                l = mid+1;
+            } else {
+                h = mid -1;
+            }
+        }
+        return -1;
+    }
+}
