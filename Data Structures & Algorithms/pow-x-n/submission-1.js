fe8ffcc6870f20227283edd9f468d47cc0baf038
Akram Mohammed <42640947+Akram-Mohammed-J@users.noreply.github.com>
2026-08-20 12:31:22 +0530

    Add: pow-x-n - submission-1

---
+++ Data Structures & Algorithms/pow-x-n/submission-1.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number} x
+     * @param {number} n
+     * @return {number}
+     */
+    myPow(x, n) {
+        function recursiveHelper(x, n) {
+            if (n === 0) return 1; // base case: anything^0 = 1
+
+            if (n < 0) {
+                return 1 / recursiveHelper(x, -n); // negative exponent = reciprocal
+            }
+
+            const half = recursiveHelper(x, Math.floor(n / 2))
+            if (n % 2 === 0) {
+                return half * half; // x^n = (x^(n/2))^2 for even n
+            } else {
+                return half * half * x; // x^n = (x^(n/2))^2 * x for odd n
+            }
+        }
+        return recursiveHelper(x, n);
+    }
+}
