bffad7cdcef7d0eee426040c987e03e1a6a862d8
Akram Mohammed <42640947+Akram-Mohammed-J@users.noreply.github.com>
2026-08-30 18:42:32 +0530

    Add: palindromic-substrings - submission-0

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    countSubstrings(s) {
+        let count = 0;
+        function expand(s, l, r) {
+            while (l >= 0 && r <= s.length && s[l] == s[r]) {
+                count++;
+                l--;
+                r++;
+            }
+        }
+        for (let i = 0; i < s.length; i++) {
+            expand(s, i, i);
+
+            expand(s, i, i + 1);
+        }
+
+        return count;
+    }
+}
