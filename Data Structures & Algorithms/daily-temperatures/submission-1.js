15217d2cee093786a8470b231c3428f8a847f91e
Akram Mohammed <42640947+Akram-Mohammed-J@users.noreply.github.com>
2026-09-04 07:29:50 +0530

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        let result = new Array(temperatures.length).fill(0);
+        let stack = [];
+
+        for (let i = 0; i < temperatures.length; i++) {
+
+            while (
+                stack.length > 0 &&
+                temperatures[stack[stack.length - 1]] < temperatures[i]
+            ) {
+                let currentIndex = stack.pop();
+
+                result[currentIndex] = i - currentIndex;
+            }
+
+            stack.push(i);
+        }
+
+        return result;
+    }
+}
